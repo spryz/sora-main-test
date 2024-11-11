3c106e2ab60cd104293a29479b05f484dca768e5
Jeff <jspry2013@gmail.com>
2024-11-10 19:10:21 -0600

    Adjusted text after face id icon on home page.

--- src/pages/Home/css.ts
+++ src/pages/Home/css.ts
@@ -71,7 +71,7 @@ const styles = StyleSheet.create({
         width: '100%',
         height: '100%',
         position: 'absolute',
-        left: setWidth(10),
+        left: setWidth(16),
         
     },
     btn_group_box:{
